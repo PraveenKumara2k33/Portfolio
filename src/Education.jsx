3bf760cde9edfca6532e80776b7dd4a75b488836
PraveenKumar-A <161425693+PraveenKumara2k33@users.noreply.github.com>
2025-04-01 16:48:29 +0530

    Update Education.jsx

--- src/Education.jsx
+++ src/Education.jsx
@@ -2,7 +2,10 @@ function Education() {
   return (
     <div>
       <h1>Education Details</h1>
-      <p>More detailed information about the education experience will be displayed here.</p>
+      <p>
+        More detailed information about the education experience will be
+        displayed here.
+      </p>
     </div>
   );
 }
