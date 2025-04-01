ae3660a5727d76c95aaa59635e62f4dec5f367b8
PraveenKumar-A <161425693+PraveenKumara2k33@users.noreply.github.com>
2025-04-01 16:47:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './Header';
-import About from './About';
-import Qualification from './QualificationPage/Qualification';
-import Skills from './Skills';
-import Project from './ProjectsPage/Project';
-import Publication from './Publication';
-import Footer from './Footer';
-import CognizantPage from './QualificationPage/CognizantPage';
-import SencholaPage from './QualificationPage/SencholaPage';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Header";
+import About from "./About";
+import Qualification from "./QualificationPage/Qualification";
+import Skills from "./Skills";
+import Project from "./ProjectsPage/Project";
+import Publication from "./Publication";
+import Footer from "./Footer";
+import CognizantPage from "./QualificationPage/CognizantPage";
+import SencholaPage from "./QualificationPage/SencholaPage";
 // import VelammalPage from './QualificationPage/VelammalPage';
 // import AyyanarPage from './QualificationPage/AyyanarPage';
-import InternpePage from './QualificationPage/InternpePage';
-import RestaurantPage from './ProjectsPage/RestaurantPage'; 
-import ChatApplicationPage from './ProjectsPage/ChatApplicationPage'; 
-import WordPressPage from './ProjectsPage/WordPressPage'; 
-import ECommercePage from './ProjectsPage/ECommercePage'; 
-import EBillPage from './ProjectsPage/EBillPage'; 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import InternpePage from "./QualificationPage/InternpePage";
+import RestaurantPage from "./ProjectsPage/RestaurantPage";
+import ChatApplicationPage from "./ProjectsPage/ChatApplicationPage";
+import WordPressPage from "./ProjectsPage/WordPressPage";
+import ECommercePage from "./ProjectsPage/ECommercePage";
+import EBillPage from "./ProjectsPage/EBillPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Home page containing all the sections
 function Home() {
   return (
     <>
-      <section id="about"><About /></section>
-      <section id="qualification"><Qualification /></section>
-      <section id="skills"><Skills /></section>
-      <section id="projects"><Project /></section>
-      <section id="publication"><Publication /></section>
+      <section id="about">
+        <About />
+      </section>
+      <section id="qualification">
+        <Qualification />
+      </section>
+      <section id="skills">
+        <Skills />
+      </section>
+      <section id="projects">
+        <Project />
+      </section>
+      <section id="publication">
+        <Publication />
+      </section>
     </>
   );
 }
